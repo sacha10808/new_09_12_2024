9ae437066c228e29df9f7ebf15b2491b9e825348
sacha10808 <sacha10sacha@gmail.com>
2024-12-09 22:14:41 +0300

    Add files via upload

---
+++ Практическое задание по уроку Базовые структуры данных.py
@@ -0,0 +1,12 @@
+#Практическое задание по уроку Базовые структуры данных
+# "1st program"
+print(9 ** 0.5 * 5)
+# "2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+# "3rd program"
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+#"4th program"
+str_ = '123.456'
+print(int(float(str_) * 10)%10)
