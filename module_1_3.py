ec3dffcc947540641124980fe083a81fcb093734
sacha10808 <sacha10sacha@gmail.com>
2024-12-09 22:17:41 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+
+#Практическая работа по уроку "Динамическая типизация"
+name = 'Александр'
+print('Name: ', name)
+age = 59
+print('Age:', age)
+new_age = age + 1
+print('New Age:', new_age)
+is_student = True
+print('Is student:', is_student)
